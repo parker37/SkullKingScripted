830ef6d3760d2f6c0506f82401335c50df5cd6d1
parker37 <rontep@icloud.com>
2023-08-03 14:45:58 -0400

    Documented all cards and fixed startup issues

--- start.lua
+++ start.lua
@@ -1,7 +1,25 @@
-card_zone_objects = getObjectFromGUID(self.guid).getObjects()
-
-flipped_cards = {}
-
 function start()
+    cardZone = getObjectFromGUID("fe0fde").getObjects()
+        -- Zone.getObjects() returns {Object, ...}
+    selectedCards = {}
 
+    for _,cards in ipairs(cardZone) do
+        
+        if(cards.is_face_down) then
+            table.insert(selectedCards, cards)
+        else
+            cards.destruct()
+        end
+    end
+    print(selectedCards)
+    deck = group(selectedCards)
+
+    deck = deck[1]
+    deck.shuffle()
+
+    deck.setPosition(getObjectFromGUID(180886).getPosition())
+
+    self.UI.setAttribute("startButton", "active", false)
+
+    
 end
